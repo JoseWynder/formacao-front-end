fdd8f5c0a935285569b57f3339f39ae7afefffcc
Jose Wynder <josewynderhernandes@gmail.com>
2026-07-28 15:19:08 -0300

    lesson(17.233): Saving values

---
+++ javascript/03-structures/js/scripts.js
@@ -0,0 +1,13 @@
+// 1 - Variables
+let nome = "Matheus";
+console.log(nome);
+
+nome = "José Wynder";
+console.log(nome);
+
+const idade = 20;
+console.log(idade);
+// idade = 21; // Error
+
+console.log(typeof nome);
+console.log(typeof idade);
