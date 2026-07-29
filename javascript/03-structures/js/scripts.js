43a57e8c80a3a1c097a63e9dba8e45daaa19c33c
Jose Wynder <josewynderhernandes@gmail.com>
2026-07-28 21:38:40 -0300

    lesson(17.250): The importance of indentation

--- javascript/03-structures/js/scripts.js
+++ javascript/03-structures/js/scripts.js
@@ -129,3 +129,14 @@ let r = 10;
 for (r; r > 0; r--) {
     console.log(`O r está diminuindo: ${r}`);
 }
+
+// 13 - Indentation
+for (let u = 0; u < 10; u++) {
+    if (u * 2 > 10) {
+        console.log(`Maior que 10! ${u}`);
+    } else {
+        if (u / 2 === 0) {
+            console.log("deu 0");
+        }
+    }
+}
