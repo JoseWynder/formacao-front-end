7de47b9b482f464283d14d1080da6df66fd18dfd
Jose Wynder <josewynderhernandes@gmail.com>
2026-07-30 23:26:18 -0300

    lesson(18.267): More about closure

--- javascript/04-functions/js/scripts.js
+++ javascript/04-functions/js/scripts.js
@@ -155,3 +155,19 @@ function someFunction() {
 }
 
 someFunction();
+
+// 10 - More about closure
+const multiplicationClosure = (n) => {
+    return (m) => {
+        return n * m;
+    };
+};
+
+const c1 = multiplicationClosure(5);
+const c2 = multiplicationClosure(10);
+
+console.log(c1);
+console.log(c2);
+
+console.log(c1(4));
+console.log(c2(5));
