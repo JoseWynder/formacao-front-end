7065457a0582650db006705654f9dbe7fb8e73ac
Jose Wynder <josewynderhernandes@gmail.com>
2026-08-01 19:52:41 -0300

    lesson(19.279): Mutation

--- javascript/05-arrays-and-objects/js/scripts.js
+++ javascript/05-arrays-and-objects/js/scripts.js
@@ -76,3 +76,21 @@ console.log(Object.keys(obj));
 console.log(Object.keys(obj2));
 console.log(Object.keys(car));
 console.log(Object.entries(car));
+
+// 9 - Mutability
+const a = {
+    name: "José"
+};
+
+const b = a;
+console.log(a);
+console.log(b);
+console.log(a === b);
+
+a.age = 31;
+console.log(a);
+console.log(b);
+
+delete b.age;
+console.log(a);
+console.log(b);
