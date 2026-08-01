f7afead8f36d9cdbe8db703fc5d54788645d342d
Jose Wynder <josewynderhernandes@gmail.com>
2026-08-01 18:54:01 -0300

    lesson(19.277): More about objects

--- javascript/05-arrays-and-objects/js/scripts.js
+++ javascript/05-arrays-and-objects/js/scripts.js
@@ -56,3 +56,17 @@ console.log(car);
 
 delete car.km;
 console.log(car);
+
+// 7 - More about objects
+const obj = {
+    a: "teste",
+    b: true
+};
+console.log(obj instanceof Object);
+
+const obj2 = {
+    c: []
+};
+Object.assign(obj2, obj);
+console.log(obj);
+console.log(obj2);
