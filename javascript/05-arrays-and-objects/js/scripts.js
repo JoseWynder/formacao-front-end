33fbab30d8e7f8e433d5bcee297cb3d368f312c3
Jose Wynder <josewynderhernandes@gmail.com>
2026-08-01 18:57:58 -0300

    lesson(19.278): Getting to know objects

--- javascript/05-arrays-and-objects/js/scripts.js
+++ javascript/05-arrays-and-objects/js/scripts.js
@@ -70,3 +70,9 @@ const obj2 = {
 Object.assign(obj2, obj);
 console.log(obj);
 console.log(obj2);
+
+// 8 - Getting to know objects
+console.log(Object.keys(obj));
+console.log(Object.keys(obj2));
+console.log(Object.keys(car));
+console.log(Object.entries(car));
