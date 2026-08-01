0bbb9a72ef27476cc3a76f67db942d62093b6aee
Jose Wynder <josewynderhernandes@gmail.com>
2026-07-31 23:24:28 -0300

    lesson(19.272): Properties

--- javascript/05-arrays-and-objects/js/scripts.js
+++ javascript/05-arrays-and-objects/js/scripts.js
@@ -5,3 +5,17 @@ console.log(typeof lista);
 
 const itens = ["Matheus", true, 2, 4.12, []];
 console.log(itens);
+
+// 2 - More about arrays
+const arr = ["a", "b", "c", "d", "e"];
+console.log(arr[0]);
+console.log(arr[2]);
+console.log(arr[83]);
+
+// 3 - Properties
+const numbers = [5, 3, 4];
+console.log(numbers.length);
+console.log(numbers["length"]);
+
+const myName = "Matheus";
+console.log(myName.length);
