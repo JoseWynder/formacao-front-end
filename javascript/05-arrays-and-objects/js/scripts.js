cc2bf597bf8e96532ef5a6b0500afaf8e76525aa
Jose Wynder <josewynderhernandes@gmail.com>
2026-08-02 21:39:10 -0300

    lesson(19.292): Join

--- javascript/05-arrays-and-objects/js/scripts.js
+++ javascript/05-arrays-and-objects/js/scripts.js
@@ -215,3 +215,11 @@ console.log(testePadEnd);
 const frase = "O rato roeu a roupa do rei de Roma";
 const arrayDaFrase = frase.split(" ");
 console.log(arrayDaFrase);
+
+// 21 - Join
+const fraseDenovo = arrayDaFrase.join(" ");
+console.log(fraseDenovo);
+
+const itensParaComprar = ["Mouse", "Teclado", "Monitor"];
+const fraseDeCompra = `Precisamos comprar: ${itensParaComprar.join(", ")}.`;
+console.log(fraseDeCompra);
