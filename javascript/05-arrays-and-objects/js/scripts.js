b45d3fb97146349ad51305467af7d2a839615279
Jose Wynder <josewynderhernandes@gmail.com>
2026-08-03 23:49:49 -0300

    lesson(19.299): JSON conversion

--- javascript/05-arrays-and-objects/js/scripts.js
+++ javascript/05-arrays-and-objects/js/scripts.js
@@ -279,3 +279,20 @@ console.log(veiculoA, veiculoB, veiculoC);
 const myJson = '{"name": "Matheus", "age": 31, "skills": ["PHP", "JavaScript", "Python"]}';
 console.log(myJson);
 console.log(typeof myJson);
+
+// 28 - JSON to object and object to JSON
+const myObject = JSON.parse(myJson);
+console.log(myObject);
+console.log(myObject.name);
+console.log(typeof myObject);
+
+// Invalid JSON
+const badJson = '{"name": Matheus, "age": 31}';
+// const myBadObject = JSON.parse(badJson);
+
+myObject.isOpenToWork = true;
+console.log(myObject);
+
+const myNewJson = JSON.stringify(myObject);
+console.log(myNewJson);
+console.log(typeof myNewJson);
