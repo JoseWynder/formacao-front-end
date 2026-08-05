17d90d7be978dfb1f9d3fa57b5c5c5e6de81e82a
Jose Wynder <josewynderhernandes@gmail.com>
2026-08-04 23:35:47 -0300

    lesson(20.307): More about prototypes

--- javascript/06-oop/js/scripts.js
+++ javascript/06-oop/js/scripts.js
@@ -36,3 +36,16 @@ console.log(Object.getPrototypeOf(bool));
 const arr = [];
 console.log(arr.length);
 console.log(Object.getPrototypeOf(arr));
+
+// 4 - More about prototype
+const myObject = {
+    a: "b",
+};
+
+console.log(Object.getPrototypeOf(myObject));
+console.log(Object.getPrototypeOf(myObject) === Object.prototype);
+
+const mySecondObject = Object.create(myObject);
+console.log(mySecondObject);
+console.log(mySecondObject.a);
+console.log(Object.getPrototypeOf(mySecondObject) === myObject);
