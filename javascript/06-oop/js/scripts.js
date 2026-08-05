5bb3f8648e95563bc7c629a84cce9f36aaa5536b
Jose Wynder <josewynderhernandes@gmail.com>
2026-08-04 23:42:10 -0300

    lesson(20.308): Basic class

--- javascript/06-oop/js/scripts.js
+++ javascript/06-oop/js/scripts.js
@@ -49,3 +49,19 @@ const mySecondObject = Object.create(myObject);
 console.log(mySecondObject);
 console.log(mySecondObject.a);
 console.log(Object.getPrototypeOf(mySecondObject) === myObject);
+
+// 5 - Basic class
+const cachorro = {
+    raca: null,
+    patas: 4,
+};
+
+const pastorAlemao = Object.create(cachorro);
+pastorAlemao.raca = "Pastor Alemão";
+
+console.log(pastorAlemao);
+console.log(pastorAlemao.patas);
+
+const bulldog = Object.create(cachorro);
+bulldog.raca = "Bulldog";
+console.log(bulldog);
