19d9a3f728279e3c75884e6604e811d0da3a3d0b
Jose Wynder <josewynderhernandes@gmail.com>
2026-08-07 23:42:37 -0300

    lesson(20.313): More about class

--- javascript/06-oop/js/scripts.js
+++ javascript/06-oop/js/scripts.js
@@ -110,3 +110,29 @@ class CachorroClasse {
 const jeff = new CachorroClasse("jeff", "Labrador");
 console.log(jeff);
 console.log(Object.getPrototypeOf(jeff));
+
+// 10 - More about class
+class Caminhao {
+    constructor(eixos, cor) {
+        this.eixos = eixos;
+        this.cor = cor;
+    }
+
+    descreverCaminhao() {
+        console.log(`Este caminhão tem ${this.eixos} eixos e é da cor ${this.cor}`);
+    }
+}
+
+const scania = new Caminhao(6, "Vermelha");
+console.log(scania);
+scania.descreverCaminhao();
+
+Caminhao.motor = 4;
+const c2 = new Caminhao(4, "Preta");
+console.log(c2);
+console.log(c2.motor);
+
+Caminhao.prototype.motor = 4.0;
+const c3 = new Caminhao(6, "Azul");
+console.log(c2);
+console.log(c3.motor);
