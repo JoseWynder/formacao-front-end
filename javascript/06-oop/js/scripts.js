137b2641c0bc120e4d3df07f2c4359911eda8fa8
Jose Wynder <josewynderhernandes@gmail.com>
2026-08-09 23:59:57 -0300

    lesson(20.315): Symbol with class

--- javascript/06-oop/js/scripts.js
+++ javascript/06-oop/js/scripts.js
@@ -152,3 +152,23 @@ console.log(Humano.prototype.idade);
 Humano.prototype.idade = "Não definida";
 console.log(matheus.idade);
 console.log(Humano.prototype.idade);
+
+// 12 - Symbol
+class Aviao {
+    constructor(marca, turbinas) {
+        this.marca = marca;
+        this.turbinas = turbinas;
+    }
+}
+
+const asas = Symbol();
+const pilotos = Symbol();
+
+Aviao.prototype[asas] = 2;
+Aviao.prototype[pilotos] = 3;
+
+const boeing = new Aviao("Boeing", 10);
+
+console.log(boeing);
+console.log(boeing[asas]);
+console.log(boeing[pilotos]);
