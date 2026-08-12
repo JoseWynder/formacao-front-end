beecda08cbb7c7d461bc1e3918f8114c98cc4dbc
Jose Wynder <josewynderhernandes@gmail.com>
2026-08-12 20:17:44 -0300

    lesson(21.329): Assertions

--- javascript/07-debug/js/scripts.js
+++ javascript/07-debug/js/scripts.js
@@ -92,3 +92,15 @@ try {
 } finally {
     console.log("O código foi executado!");
 }
+
+// 8 - Assertion
+function checkArray(arr) {
+    if (arr.length === 0) {
+        throw new Error("O array precisa ter elementos");
+    } else {
+        console.log(`O array tem ${arr.length} elementos`);
+    }
+}
+
+// checkArray([]);
+checkArray([1, 2, 3]);
