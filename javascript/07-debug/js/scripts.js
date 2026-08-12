76ddbf962ef01bd0dbba3a5ab15a031f0a987fc7
Jose Wynder <josewynderhernandes@gmail.com>
2026-08-12 19:56:57 -0300

    lesson(21.324): Using the debugger

--- javascript/07-debug/js/scripts.js
+++ javascript/07-debug/js/scripts.js
@@ -25,3 +25,24 @@ if (a > 5) {
     a = 25;
 }
 console.log(a);
+
+// 3 - Debugger
+let c = 1;
+let d = 2;
+
+if (c == 1) {
+    c = d + 2;
+}
+
+// debugger;
+
+for (let i = 0; i < d; i++) {
+    c += 2;
+}
+console.log("executou o loop");
+
+// debugger;
+
+if (c > 5) {
+    c = 25;
+}
