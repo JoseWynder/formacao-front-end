d36d48b6183d7d1b839eb1943e90bab660ff18f3
Jose Wynder <josewynderhernandes@gmail.com>
2026-08-16 19:08:36 -0300

    lesson(22.341): Generators

--- javascript/08-async/js/scripts.js
+++ javascript/08-async/js/scripts.js
@@ -108,3 +108,15 @@ async function chamadaAsync() {
 }
 
 chamadaAsync();
+
+// 9 - Generators
+function* generator() {
+    yield 1;
+    yield 2;
+    yield 3;
+}
+
+const gen = generator();
+console.log(gen.next().value);
+console.log(gen.next().value);
+console.log(gen.next().value);
